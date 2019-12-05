54031f3c078ba946371609168088d95bbc40577b
junior <junior.c854@stu.nebo.edu>
2019-12-05 10:29:45 -0700

    player controler

--- Jack's Journey/Assets/Scripts/MoveForward.cs
+++ Jack's Journey/Assets/Scripts/MoveForward.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MoveForward : MonoBehaviour
 {
     public float speed = 20;
+    public float bound = 20;
+    public bool forward = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,18 @@ public class MoveForward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+        if (forward)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+        }else
+        {
+            transform.Translate(Vector3.right * -speed * Time.deltaTime, Space.World);
+        }
+        
+
+        if (transform.position.x > bound)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Jack's Journey/Assets/Scripts/PlayerControler.cs
+++ Jack's Journey/Assets/Scripts/PlayerControler.cs
@@ -5,34 +5,32 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     private Rigidbody playerRB;
+    public GameObject projectile;
     public float jumpForce = 10;
-    public float xrange = 20;
+    public float gravitymod = 10;
     public float speed = 5;
     public float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravitymod;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -xrange)
-        {
-            transform.position = new Vector3(-xrange, transform.position.y, transform.position.z);
-        }
-        if (transform.position.x > xrange)
-        {
-            transform.position = new Vector3(xrange, transform.position.y, transform.position.z);
-        }
-
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectile, new Vector3(transform.position.x + 1, transform.position.y, transform.position.z), projectile.transform.rotation);
         }
     }
 }
